94bb646a1885d122431908961fe887576d4e278a
yura <yurik1776@gmail.com>
2019-03-22 18:22:52 +0200

    added mongo ecto example

--- lib/simple_server/auth_controller.ex
+++ lib/simple_server/auth_controller.ex
@@ -5,8 +5,9 @@ defmodule SimpleServer.AuthController do
   import Plug.Conn
 
   def add_weather(conn) do
-    Weather.new_changeset(%Weather{}, %{city: "Netanya", temp_lo: 10, temp_hi: 60, prcp: 70})
-    |> Repo.insert!()
+    %Weather{}
+    |> Weather.new_changeset(%{city: "Netanya", temp_lo: 10, temp_hi: 60, prcp: 70})
+    |> Repo.insert!
     |> Poison.encode!
     |> (&send_resp(conn, 200, &1)).()
   end
